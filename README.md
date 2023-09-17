9893cf96539d49ead4303742139fef862625da45
FogonaBabilonia <86286539+FogonaBabilonia@users.noreply.github.com>
2023-09-17 15:17:31 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# tlauncher-install
+# TLauncher_Install!
+simple shell script to install tlauncher for minecraft in your linux machine
+## Instalation
+First you will need java runtime environment installed.
+So if you do not have it yet, install it with one of the following commands:
+### debian/ubuntu/linux mint
+	sudo apt install default-jre
+### arch linux
+	sudo pacman -S jre-openjdk
+### fedora
+	sudo dnf install java-latest-openjdk.x86_64
+
+after installing java run the following commands:
+
+	git clone https://github.com/FogonaBabilonia/tlauncher-install.git
+	cd tlauncher-install
+	bash install.sh
+
+After that you should be able to find tlauncher in your applications menu:
+![tlauncher in the apps menu](https://i.imgur.com/9ddvivx.png)
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+download_tl() {
+	curl https://dl2.tlauncher.org/f.php?f=files/TLauncher-2.885.zip -o "TLauncher.zip"
+	unzip TLauncher.zip -x README-EN.txt README-RUS.txt
+}
+
+download_icon() {
+	wget https://i.imgur.com/yN4ciBD.png
+}
+
+create_entry() {
+	echo "
+[Desktop Entry]
+Name=TLauncher
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=false
+Exec=java -jar $HOME/.tl_minecraft/TLauncher-2.885.jar
+Icon=$HOME/.tl_minecraft/yN4ciBD.png" > tlauncher.desktop
+	cp tlauncher.desktop $HOME/.local/share/applications
+}
+
+set_files() {
+	mkdir $HOME/.tl_minecraft
+	cp TLauncher-2.885.jar $HOME/.tl_minecraft
+	cp yN4ciBD.png $HOME/.tl_minecraft
+
+	rm TLauncher.zip TLauncher-2.885.jar yN4ciBD.png tlauncher.desktop
+}
+
+if  ! [[ -x "$(command -v java -version)" ]]; then
+	echo "you do not have java installed"
+	exit 1
+fi
+
+download_tl
+download_icon
+create_entry
+set_files
